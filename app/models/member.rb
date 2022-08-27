f236b5bf39495ded5dd4992502671a1c9f6a3fdb
Tad <a@as-MacBook-Pro.local>
2022-08-27 13:41:37 -0400

    add validation rules for members model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,7 @@
 class Member < ApplicationRecord
+
+    validates :email, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates_inclusion_of :subscribed, :in => [true, false]
+
 end
